f97d2f591215415c248e3aedb15a0c5306765c32
brycen-q-hang <daoquanghang.hue@gmail.com>
2024-11-25 10:34:06 +0700

    update

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -9,4 +9,14 @@ public class HelloController {
     public String index() {
         return "Hello World with Hoi Dan IT!";
     }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user can access this page";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admin can access this page";
+    }
 }
