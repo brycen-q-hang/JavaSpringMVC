86adafbac4eb6921a651da5d3f405b9867d136fa
brycen-q-hang <daoquanghang.hue@gmail.com>
2024-11-27 10:06:14 +0700

    update76

--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -12,10 +12,15 @@ public class User {
     private long id;
 
     private String email;
+
     private String password;
     private String fullName;
     private String address;
     private String phone;
+
+    private String avatar;
+
+    // roleId
 
     public long getId() {
         return id;
@@ -65,10 +70,18 @@ public class User {
         this.phone = phone;
     }
 
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
     @Override
     public String toString() {
         return "User [id=" + id + ", email=" + email + ", password=" + password + ", fullName=" + fullName
-                + ", address=" + address + ", phone=" + phone + "]";
+                + ", address=" + address + ", phone=" + phone + ", avatar=" + avatar + "]";
     }
 
 }
